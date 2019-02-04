4591ce3526337c2b8cabd8b72051b0591150c19c
Alex Zdatny <alexzdatny@gmail.com>
2019-02-04 23:15:53 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,6 +4,7 @@ describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
     @post.author = Author.create(name: "Steven")
+    @post.save
   end
 
   it 'shows the title on the show page in a h1 tag' do
